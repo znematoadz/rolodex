880072d15176f422357019abf794b6a3793778e9
Alex J <jones6211@gmail.com>
2019-09-01 13:37:00 -0500

    Updates

---
+++ precache-manifest.b5730f0ddd9e509c00ea68faca369f9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c45fd849cf6eb94eb2372ed88e574b8",
+    "url": "/rolodex/index.html"
+  },
+  {
+    "revision": "75e5adf56941a1725560",
+    "url": "/rolodex/static/css/main.4f3ef24d.chunk.css"
+  },
+  {
+    "revision": "4a550877d1bfac977f0f",
+    "url": "/rolodex/static/js/2.94537022.chunk.js"
+  },
+  {
+    "revision": "75e5adf56941a1725560",
+    "url": "/rolodex/static/js/main.792d7698.chunk.js"
+  },
+  {
+    "revision": "5798016e672d2c7b9931",
+    "url": "/rolodex/static/js/runtime~main.c7006c47.js"
+  }
+]);
